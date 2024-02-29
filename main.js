a5e190fe5b9f8f2229067ee4ba77f36ce5878e78
Alex N <77512968+AlexNelson52@users.noreply.github.com>
2024-02-29 11:59:09 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+`use strict`;
+let chamber = ["Nothing","Bullet"];
+
+function gun(survivalOrDeath){
+  var colore = document.getElementById("flash-red").value;
+  
+  for (let i = 0; i <= chamber.length; i++){
+    let shooting = chamber[Math.floor(Math.random() * chamber.length)];
+    if (shooting === "Nothing"){
+      document.body.style.backgroundColor = "black";
+      return alert("Nothing Happens.")} 
+      else { 
+        document.body.style.backgroundColor = "red";
+        return alert("Oh dear you are dead!")
+    }
+  }
+}
+
+gun()
+
